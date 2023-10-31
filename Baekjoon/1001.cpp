0dbdccc5a5303aa4f1df4bae9e8f248f4b340571
sonnyzxc <sonnyku9@gmail.com>
2023-10-31 11:38:59 +0000

    Created cp template and solved some Baekjoon problems

---
+++ Baekjoon/1001.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+#define endl "\n";
+#define ff first
+#define ss second
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+  cout << A - B << endl;
+}
---
+++ Baekjoon/2557.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+#define endl "\n";
+#define ff first
+#define ss second
+
+int main() {
+  cout << "Hello World!" << endl;
+}
