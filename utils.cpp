dcda8ee85e4c849ed88e4fea1781e66f7ca93e17
Sonny <69658757+sonnyzxc@users.noreply.github.com>
2024-12-28 09:44:33 +0000

    feat: added BFS, DFS to utils.cpp

--- utils.cpp
+++ utils.cpp
@@ -3,9 +3,14 @@ using namespace std;
 
 #define vi vector<int>
 
+/**
+ * Serves as my notes for competitive programming, and
+ * a collection of helper functions.
+ */
+
 bool isPrime(int num) {
     if (num <= 1) return 0;
-    for (int i = 2; i * i <= num; i++) 
+    for (int i = 2; i * i <= num; i++)
         if (num % i == 0) return 0;
     return 1;
 }
@@ -21,4 +26,34 @@ int binarySearch(int x, int N, const vi &Li) {
         else R = mid - 1;
     }
     return 0;
+}
+
+void BFS(int k, int N) {
+    vector<vi> adj(N + 1);
+    queue<int> q;
+    q.push(k);
+    vector<bool> visited(N + 1, 0);
+    visited[1] = k;
+
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop();
+
+        for (int n : adj[cur]) {
+            if (!visited[n]) {
+                visited[n] = 1;
+                q.push(n);
+            }
+        }
+    }
+}
+
+/* Recursive DFS. Can use stack for iterative method. */
+void DFS(int V, unordered_map<int, vi> &m, vector<bool> &DFSvisited) {
+    int cur = V;
+    DFSvisited[cur] = 1;
+
+    for (auto &x : m[cur]) {
+        if (!DFSvisited[x]) DFS(x, m, DFSvisited);
+    }
 }
