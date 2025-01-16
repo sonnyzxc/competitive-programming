9364c71d7651622fda34e27a198ae7cd6b386b6d
Sonny <69658757+sonnyzxc@users.noreply.github.com>
2025-01-16 07:55:33 +0000

    feat: finished 23825.

---
+++ Baekjoon/23825.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+#define ff first
+#define ss second
+#define vi vector<int>
+#define pii pair<int, int>
+#define pis pair<int, string>
+#define endl "\n"
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N, M;
+    cin >> N >> M;
+    cout << min(N, M) / 2 << endl;
+}
