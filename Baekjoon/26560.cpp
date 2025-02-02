3cfd339f534d7638653c5e565ec5ad3b4afc298d
Sonny <69658757+sonnyzxc@users.noreply.github.com>
2025-02-02 04:38:58 +0000

    feat: finished 26560.

---
+++ Baekjoon/26560.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+#define ff first
+#define ss second
+#define vi vector<int>
+#define pii pair<int, int>
+#define pis pair<int, string>
+#define endl "\n"
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+    cin.ignore();
+
+    while (N--) {
+        string S;
+        getline(cin, S);
+        cout << (S.back() == '.' ? S : S + ".") << endl;
+    }
+}
